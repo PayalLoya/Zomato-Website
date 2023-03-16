29a0f00a7d37fd2e3f4f1565b75bc8a584f0cb86
Payal Loya <payalloya20@gmail.com>
2023-03-16 22:08:10 +0530

    added auth,booking

---
+++ React/zomato-app/src/components/Booking/ViewOrder.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import OrderDisplay from "../Booking/OrderDisplay";
+import axios from "axios";
+
+const url = "https:///zomato-api-ntso.onrender.com";
+
+export default class ViewOrder extends Component {
+  constructor() {
+    super();
+    this.state = {
+      orders: "",
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <OrderDisplay orderData={this.state.orders} />
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    let email = sessionStorage.getItem("userInfo")
+      ? sessionStorage.getItem("userInfo").split(",")[1]
+      : [];
+    axios.get(`${url}/orders?email=${email}`).then((res) => {
+      this.setState({ orders: res.data });
+    });
+  }
+}
